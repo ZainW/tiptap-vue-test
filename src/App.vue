df6a00afa910eb009a3c043b0f9b31f02664f2d4
Zain Wania <zain@wania.dev>
2024-04-22 10:38:00 -0400

    microsoft and netflix

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,24 @@ import Tiptap from "@/components/tiptap.vue";
     <div class="max-w-full">
       <Tiptap />
     </div>
-    <img
-      src="https://www.google.ca/logos/doodles/2024/earth-day-2024-6753651837110453-law.gif"
-      alt="earth day"
-    >
+    <div>
+      <img
+        src="https://www.google.ca/logos/doodles/2024/earth-day-2024-6753651837110453-law.gif"
+        alt="earth day"
+      >
+    </div>
+    <div>
+      <img
+        src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/756924_Merch-CP-0_1668x940_02?wid=380&hei=213&fit=crop"
+        alt="microsoft"
+      >
+    </div>
+    <div>
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+        alt="netflix"
+      >
+    </div>
   </div>
 </template>
 
